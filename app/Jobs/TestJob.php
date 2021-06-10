10fcd16e633f66a760b80c3ded5415d0ad37d07c
PatrickDCullen <cullenpatrickd@gmail.com>
2021-06-10 14:30:20 -0400

    End Ep 11: Configurations Reference

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -5,25 +5,38 @@ namespace App\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class TestJob implements ShouldQueue, ShouldBeEncrypted
+class TestJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $secret;
+    public $connection = 'redis';
+    public $queue = 'notifications';
+    public $backoff = 30; // or array or method
+    public $timeout = 60;
+    public $tries = 3;
+    public $delay = 300;
+    public $afterCommit = true;
+    public $shouldBeEncrypted = true;
+
+    public $uniqueId = 'products';
+    public $uniqueFor = 10;
+
+    public $deleteWhenMissingModels = true;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($secret)
+    public function __construct()
     {
-        $this->secret = $secret;
+
     }
 
     /**
@@ -34,5 +47,15 @@ class TestJob implements ShouldQueue, ShouldBeEncrypted
     public function handle()
     {
         //
+    }
+
+    public function uniqueId()
+    {
+        return $this->product->id;
+    }
+
+    public function retryUntil()
+    {
+        return now()->addDay();
     }
 }
